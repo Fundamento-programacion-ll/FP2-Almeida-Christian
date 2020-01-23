5140a204f235704ccbb59fb940ce466ba719f31c
Christian <almeida.christian708@gmail.com>
2020-01-23 08:25:55 -0500

    TECTO NUEVO

---
+++ JavaApplication4/src/numeros/numeros.java
@@ -0,0 +1,34 @@
+package numeros;
+
+public class numeros {
+    
+double num;
+int cant;
+double  valor;
+
+    public double getNum() {
+        return num;
+    }
+
+    public void setNum(double num) {
+        this.num = num;
+    }
+
+    public int getCant() {
+        return cant;
+    }
+
+    public void setCant(int cant) {
+        this.cant = cant;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+
+}
